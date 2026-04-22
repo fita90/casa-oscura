797301eb40b2d5678d58f97cacb9bd19e9b5b3e4
Fernando Tovar <81651533+fita90@users.noreply.github.com>
2026-04-22 01:32:32 -0400

    Use Cool Material image for House Negroni

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.foodandwine.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'coolmaterial.com',
+      },
     ],
   },
 }
