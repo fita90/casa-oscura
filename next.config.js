d8126837e77dd1fd0d646c6b5c3a378d211e410b
Fernando Tovar <81651533+fita90@users.noreply.github.com>
2026-04-22 01:30:17 -0400

    Use Food and Wine image for French 75

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'barossadistilling.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.foodandwine.com',
+      },
     ],
   },
 }
